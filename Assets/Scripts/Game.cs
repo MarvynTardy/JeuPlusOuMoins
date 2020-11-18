4ec318048d8c4b872c57101634f8ec88d9819ffc
Marvyn Tardy <marvyn.tardy@e-artsup.net>
2020-11-18 10:09:48 +0100

    FirstCommit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField]
+    public InputField playerValueField;
+    private TweenAnimation m_tweenAnimation;
+    private int m_PlayerValue = 0;
+    private int m_valueGame = 0;
+
+    private void Awake()
+    {
+        m_tweenAnimation = FindObjectOfType<TweenAnimation>();
+    }
+    void Start()
+    
+    {
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_PlayerValue = (int.Parse(playerValueField.text));
+
+            if (m_PlayerValue < m_valueGame)
+            {
+                Debug.Log("C'est plus");
+                StartCoroutine(m_tweenAnimation.More());
+            }
+            if (m_PlayerValue > m_valueGame)
+            {
+                Debug.Log("C'est moins");
+                StartCoroutine(m_tweenAnimation.Less());
+            }
+            if(m_PlayerValue == m_valueGame)
+            {
+                Debug.Log("C'est ça !! GG");
+                m_tweenAnimation.Win();
+            }
+            if (playerValueField.text == null)
+            {
+                Debug.Log("Entre une valeur, Boloss");
+            }
+        }
+
+        
+       
+        
+    }
+
+    public void CreateValue()
+    {
+        m_valueGame = Random.Range(1, 101);
+        Debug.Log(m_valueGame);
+    }
+
+    public void Validate()
+    {
+        m_PlayerValue = (int.Parse(playerValueField.text));
+
+        if (m_PlayerValue < m_valueGame)
+        {
+            Debug.Log("C'est plus");
+            StartCoroutine(m_tweenAnimation.More());
+        }
+        if (m_PlayerValue > m_valueGame)
+        {
+            Debug.Log("C'est moins");
+            StartCoroutine(m_tweenAnimation.Less());
+        }
+        if (m_PlayerValue == m_valueGame)
+        {
+            Debug.Log("C'est ça !! GG");
+            m_tweenAnimation.Win();
+        }
+        if (playerValueField.text == null)
+        {
+            Debug.Log("Entre une valeur, Boloss");
+        }
+    }
+
+
+
+}
+
---
+++ Assets/Scripts/TweenAnimation.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TweenAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform m_ButtonPlay, m_ButtonQuit, m_PlayerValueField, m_MoreText, m_LessText, m_Title, m_TutoText, m_ButtonReturnMenu, m_ButtonValidate, m_WinText;
+    private Game m_Game;
+
+    private void Awake()
+    {
+        m_Game = FindObjectOfType<Game>();
+    }
+    void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        
+    }
+    public void Play()
+    {
+        m_ButtonPlay.DOAnchorPos(new Vector2(500, -105), 0.25f);
+        m_ButtonQuit.DOAnchorPos(new Vector2(-500, -150), 0.25f);
+        m_PlayerValueField.DOAnchorPos(new Vector2(0, -30), 0.25f);
+        m_Title.DOAnchorPos(new Vector2(0, -350), 0.25f);
+        m_TutoText.DOAnchorPos(new Vector2(0, 20),0.25f);
+        m_ButtonValidate.DOAnchorPos(new Vector2(0, -105), 0.25f);
+        m_ButtonReturnMenu.DOAnchorPos(new Vector2(0, -150), 0.25f);
+        m_Game.CreateValue();
+
+    }
+    
+    public IEnumerator Less()
+    {
+        m_LessText.DOAnchorPos(new Vector2(-200, -110), 0.25f);
+        yield return new WaitForSeconds(1f);
+        m_LessText.DOAnchorPos(new Vector2(-500, -110), 0.25f);
+    }
+    public IEnumerator More()
+    {
+        m_MoreText.DOAnchorPos(new Vector2(200, -110), 0.25f);
+        yield return new WaitForSeconds(1f);
+        m_MoreText.DOAnchorPos(new Vector2(500, -110), 0.25f);
+        
+            
+
+
+    }
+    public void Validate()
+    {
+        
+    }
+    public void ReturnMenu()
+    {
+        m_ButtonPlay.DOAnchorPos(new Vector2(0, -105), 0.25f);
+        m_ButtonQuit.DOAnchorPos(new Vector2(0, -150), 0.25f);
+        m_ButtonValidate.DOAnchorPos(new Vector2(-500, -105), 0.25f);
+        m_ButtonReturnMenu.DOAnchorPos(new Vector2(500, -150), 0.25f);
+        m_TutoText.DOAnchorPos(new Vector2(0, 300), 0.25f);
+        m_Title.DOAnchorPos(new Vector2(0, 45), 0.25f);
+        m_PlayerValueField.DOAnchorPos(new Vector2(0, 250), 0.25f);
+        m_WinText.DOAnchorPos(new Vector2(0, 400), 0.25f);
+
+    }
+
+    public void Win()
+    {
+        m_TutoText.DOAnchorPos(new Vector2(0, 300), 0.25f);
+        m_PlayerValueField.DOAnchorPos(new Vector2(0, 250), 0.25f);
+        m_ButtonValidate.DOAnchorPos(new Vector2(-500, -105), 0.25f);
+        m_WinText.DOAnchorPos(new Vector2(0, 0), 0.25f);
+    }
+
+
+
+}
